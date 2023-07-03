0bb893064e425d7fca3d87163baaa37e6a828123
fahim2204 <fahimfaisal1998@gmail.com>
2023-07-04 02:14:28 +0600

    Contact Page Modified

--- pages/contact.js
+++ pages/contact.js
@@ -1,87 +1,88 @@
 import React from "react";
 import { BiGlobe } from "react-icons/bi";
 import { MdLocationOn, MdPhone } from "react-icons/md";
-import contactHeroImage from "../public/assets/contactHero.svg";
-// import PurpleNewmorfButton from "../public/assets/PurpleNewmorfButton";
 
 import Image from "next/image";
-import CustomInput from "/components/CustomInput";
 import MainLayout from "../layouts/MainLayout";
 
-const contact = () => {
+const Contact = () => {
   return (
     <MainLayout>
-      <div className=" lg:py-8 py-4 text-black">
-        <h1 className="lg:text-4xl text-2xl font-semibold text-center lg:mb-5 mb-2">
-          Have Some Question?
-        </h1>
-        <p className="text-center mb-10 lg:text-base text-xs">
-          Please feel free to talk to use you have ay questions.
-          <br />
-          We endeavor at answer withing 24 hours.
-        </p>
-        <div className="flex lg:flex-row flex-col">
-          <div className="lg:w-7/12 w-full lg:pr-20">
-            <div>
-              <Image src={contactHeroImage} alt="" />
-            </div>
-            <div className="mt-10">
-              <h3 className="font-semibold lg:text-4xl text-2xl mb-8">Get in touch</h3>
-              <ul>
-                <li className="my-5 text-md flex items-center justify-start">
-                  <MdLocationOn className="lg:text-4xl text-2xl mr-5 text-beta-900" />{" "}
-                  {"Lorem ipsum dolor sit amet,"}
-                </li>
-                <li className="my-5 text-md flex items-center justify-start">
-                  <MdPhone className="lg:text-4xl text-2xl mr-5 text-beta-900" />{" "}
-                  {"+91 0000000"}
-                </li>
-                <li className="my-5 text-md flex items-center justify-start">
-                  <BiGlobe className="lg:text-4xl text-2xl mr-5 text-beta-900" />{" "}
-                  {"bikefixup@gmail.com"}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div className="lg:w-5/12 w-full">
-            <form className="heroFormGlassDiv px-5 py-2 md:px-10 md:py-3 ">
-              <input
-                name="name"
-                placeholder="Name"
-                class="my-2 w-full h-[45px] px-[27px] py-1 formGlassInput text-black"
-                type="text"
-              />
-              <input
-                name="phone"
-                placeholder="Phone"
-                class="my-2 w-full h-[45px] px-[27px] py-1 formGlassInput text-black"
-                type="number"
-              />
-              <input
-                name="email"
-                placeholder="Email"
-                class="my-2 w-full h-[45px] px-[27px] py-1 formGlassInput text-black"
-                type="email"
-              />
-              <textarea
-                name="message"
-                placeholder="Message"
-                class="my-2 w-full mb-8 h-[120px] px-[27px] py-1 formGlassInput text-black"
-              ></textarea>
-              <div className="flex justify-center items-center mb-8">
-                <button className="newMorfPurple  px-16 py-2  text-white">
-                  <span className="flex gap-2 items-center justify-center">
-                    {" "}
-                    Send{" "}
-                  </span>
-                </button>
-              </div>
-            </form>
+      <h1 className="lg:text-4xl text-2xl font-semibold text-center lg:mb-5 mb-2 text-black">
+        Have Some Question?
+      </h1>
+      <p className="text-center mb-8 lg:text-base text-xs text-black">
+        Please feel free to talk to use you have ay questions.
+        <br />
+        We endeavor at answer withing 24 hours.
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 text-black">
+        <div className="">
+            <Image
+              src="/image/contact-hero.webp"
+              height={280}
+              width={415}
+              priority
+              alt="Contact - BikeFixup"
+            />
+          <div className="mt-10">
+            <h3 className="font-semibold lg:text-4xl text-2xl mb-8">
+              Get in touch
+            </h3>
+            <ul className="text-md flex-col space-y-2">
+              <li className="flex items-center justify-start">
+                <MdLocationOn className="lg:text-4xl text-2xl mr-3 text-beta-900" />
+                Lorem ipsum dolor sit amet,
+              </li>
+              <li className="flex items-center justify-start">
+                <MdPhone className="lg:text-4xl text-2xl mr-3 text-beta-900" />
+                +91 0000000
+              </li>
+              <li className="flex items-center justify-start">
+                <BiGlobe className="lg:text-4xl text-2xl mr-3 text-beta-900" />
+                bikefixup@gmail.com
+              </li>
+            </ul>
           </div>
         </div>
+        <form className="mb-8 px-10 pt-8 h-auto bg-gradient-to-b from-white/20 to-white/0 bg-white/10 rounded-3xl shadow-md">
+          <input
+            id="name"
+            name="name"
+            placeholder="Name"
+            className="px-8 py-5 w-full my-2 formGlassInput"
+            type="text"
+          />
+          <input
+            name="phone"
+            placeholder="Phone"
+            className="px-8 py-5 w-full my-2 formGlassInput hide-number-buttons"
+            type="number"
+            min={1}
+          />
+          <input
+            name="email"
+            placeholder="Email"
+            className="px-8 py-5 w-full my-2 formGlassInput"
+            type="email"
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            className="px-8 py-5 w-full my-2 formGlassInput"
+            rows={5}
+          ></textarea>
+          <div className="w-full flex justify-center items-center my-6">
+            <button className="newMorfPurple text-center py-4 px-10 text-white font-semibold ">
+              <span className="flex gap-2 items-center justify-center">
+                Send
+              </span>
+            </button>
+          </div>
+        </form>
       </div>
     </MainLayout>
   );
 };
 
-export default contact;
+export default Contact;
